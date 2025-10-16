326d5f5cc933fb0bcee87f0bac3b27a809194440
mehedishishir0 <mehedihasanshishir.info@gmail.com>
2025-10-16 16:07:35 +0600

    first commit

---
+++ src/app/(auth)/forgot-password/_components/forgetPassword.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { toast } from "sonner";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+
+const formSchema = z.object({
+  email: z.string().min(1, "Email is required"),
+});
+
+export default function ForgetPassword() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    try {
+      console.log(values);
+      toast.success("Login successful!", {
+        description: `Welcome back, ${values.email}!`,
+      });
+    } catch (error) {
+      console.error("Form submission error", error);
+      toast.error("Failed to submit the form. Please try again.");
+    }
+  }
+
+  return (
+    <div
+      className="relative min-h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: "url('/bg.jpg')",
+      }}
+    >
+      <div className="absolute inset-0 bg-white/60"></div>
+
+      <div className="relative z-10 bg-white/85 backdrop-blur-sm rounded-2xl shadow-md p-8 sm:p-10 w-full max-w-[500px]">
+        <div className="mb-10">
+          <h3 className="text-[#00383B] text-center text-[40px] ">Forgot Password</h3>
+          <p className="text-[#6C757D] font-normal text-[16px] text-center">Enter your email to recover your password</p>
+        </div>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-[16px]"
+          >
+            {/* Email */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-[#616161] font-medium text-[16px] mb-2">
+                    Email Address
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      type="email"
+                      {...field}
+                      placeholder="hello@example.com"
+                      className="border border-[#616161] py-4 focus-visible:ring-0 focus-visible:border-[#147575]"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              type="submit"
+              className="bg-[#00383B] hover:bg-[#00383B]/90 w-full text-white text-[16px] py-5 shadow-md hover:shadow-lg transition-shadow"
+            >
+              Send OTP
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Poppins } from 'next/font/google';
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+
+const geistSans = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
   variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
 });
 
 export const metadata: Metadata = {
@@ -26,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.className}  antialiased`}
       >
         {children}
       </body>
